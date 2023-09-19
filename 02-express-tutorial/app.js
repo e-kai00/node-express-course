bd5ac1c750b5d6bdbf59a317e423cb1541f1c3d0
Katerina <yelkaterina@gmail.com>
2023-09-19 15:17:56 +0200

    methods, routers and controllers

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,19 +1,25 @@
 const express = require('express')
-const path = require('path')
-
 const app = express()
+const people = require('./routes/people')
+const auth = require('./routes/auth')
 
-// static and middleware
-app.use(express.static('./public'))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({extended: false}))
+// parse json
+app.use(express.json())
+app.use('/api/people', people)
+app.use('/login', auth)
 
-app.all('*', (req, res) => {
-    res.status(404).send('resource is not found')
-})
+
+
+
+
+
+
 
 app.listen(5000, () => {
-    console.log('server is listening on port 5000...')
+    console.log('Server is listening on port: 5000...')
 })
---
+++ 02-express-tutorial/autherize.js
@@ -0,0 +1,12 @@
+const autherize = (req, res, next) => {
+    const {user} = req.query
+    if (user === 'john') {
+        req.user = {name: 'john', id: 3}
+        next()
+    }
+    else {
+        res.status(401).send('Unautherized')
+    }
+}
+
+module.exports = autherize
