1ca86d7acf17ea073d04985a4b73d56e66f95ef3
Katerina <yelkaterina@gmail.com>
2023-09-22 16:53:18 +0200

    Set up custom 404

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,6 +3,7 @@ const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
+const notFound = require('./middleware/not-found')
 
 // middleware (.json() to get data in req.body)
 app.use(express.static('./public'))
@@ -10,6 +11,7 @@ app.use(express.json())
 
 // router 
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
 
 const port = 3000
 
