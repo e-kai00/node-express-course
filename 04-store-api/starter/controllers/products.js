2ebbbc3a42acb4e95a2da88f20c45a4fe1a84cc2
Katerina <yelkaterina@gmail.com>
2023-09-27 19:28:57 +0200

    Set up basic find function

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,9 +1,13 @@
+const Product = require('../models/product')
+
 const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({msg: 'Product testing route.'})
+    const products = await Product.find({name: "a first wooden table",})
+    res.status(200).json({products, nbHits: products.length})
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({msg: 'Product route.'})
+    const products = await Product.find(req.query)
+    res.status(200).json({products, nbHits: products.length})
 }
 
 module.exports = {
