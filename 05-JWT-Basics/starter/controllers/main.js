0f001a574a889b78c8af764111bbf085b443453a
Katerina <yelkaterina@gmail.com>
2023-10-02 19:02:14 +0200

    Set up JWT

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,10 +1,25 @@
-const login = (req, res) => {
-    res.send('Fake Login/Register/Signup Route')
+const { use } = require("express/lib/router")
+const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
+
+const login = async (req, res) => {
+   const {username, password} = req.body
+   
+   if (!username || !password) {
+    throw new CustomAPIError('Please provide username and password', 400)
+   }
+   // for demo; id will be provided by DB
+   const id = new Date().getDate()
+
+   // provide: payload, secret, options
+   const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'})
+
+    res.status(200).json({msg: 'user created', token})
 }
 
 const dashboard = (req, res) => {
     const luckyNumber = Math.floor(Math.random()*100)
-    res.status(200).json({msg: `Hello, Ktie`, secret: `Here is ur secret data. Your lucky number is ${{luckyNumber}}` })
+    res.status(200).json({msg: `Hello, Ktie`, secret: `Here is ur secret data. Your lucky number is ${luckyNumber}` })
 }
 
 module.exports = {login, dashboard}
