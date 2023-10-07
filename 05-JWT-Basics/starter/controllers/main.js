ce8bb4fa33c9d51b975458492fa2da7203577fbe
Katerina <yelkaterina@gmail.com>
2023-10-07 15:09:38 +0200

    Add middleware to verify token

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -2,6 +2,7 @@ const { use } = require("express/lib/router")
 const CustomAPIError = require('../errors/custom-error')
 const jwt = require('jsonwebtoken')
 
+
 const login = async (req, res) => {
    const {username, password} = req.body
    
@@ -17,25 +18,12 @@ const login = async (req, res) => {
     res.status(200).json({msg: 'user created', token})
 }
 
+
 const dashboard = async (req, res) => {
-    const authHeader = req.headers.authorization
+    console.log(req.user)
 
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-        throw new CustomAPIError ('No token provided', 401)
-    }
-
-    const token = authHeader.split(' ')[1]
-
-    // varify token
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)        
-        const luckyNumber = Math.floor(Math.random()*100)
-        res.status(200).json({msg: `Hello, ${decoded.username}`, secret: `Here is ur secret data. Your lucky number is ${luckyNumber}` })
-    } catch (error) {
-        throw new CustomAPIError('Not authorized to access this route', 401)
-    }
-
-
+    const luckyNumber = Math.floor(Math.random()*100)
+    res.status(200).json({msg: `Hello, ${req.user.username}`, secret: `Here is ur secret data. Your lucky number is ${luckyNumber}` })
 }
 
 module.exports = {login, dashboard}
