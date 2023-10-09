87aa220e091c00a80a9945514826914dd491be96
Katerina <yelkaterina@gmail.com>
2023-10-09 18:02:42 +0200

    Hash password and create user

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,9 +1,21 @@
+const User = require('../models/User')
+const {StatusCodes} = require('http-status-codes')
+const bcrypt = require('bcryptjs')
+
+
 const register = async (req, res) => {
-    req.send('register user')
+    const {name, email, password} = req.body
+    // hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    const tempUser = {name, email, password: hashedPassword}
+    // create user
+    const user = await User.create({...tempUser})
+    res.status(StatusCodes.CREATED).json({user})
 }
 
 const login = async (req, res) => {
-    req.send('login user')
+    res.send('login user')
 }
 
 module.exports = {
