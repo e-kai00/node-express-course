fab7a38d2fcbd6fece90ee17328397b3c040c4ac
Katerina <yelkaterina@gmail.com>
2023-10-13 17:37:51 +0200

    Set up login functionality

--- 06-jobs-api/starter/controllers/auth.js
+++ 06-jobs-api/starter/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require('../models/User')
 const {StatusCodes} = require('http-status-codes')
+const {BadRequestError, UnauthenticatedError} = require('../errors')
+const { use } = require('express/lib/router')
 
 
 const register = async (req, res) => {
@@ -10,9 +12,27 @@ const register = async (req, res) => {
     res.status(StatusCodes.CREATED).json({user: {name: user.name}, tocken})
 }
 
+
 const login = async (req, res) => {
-    res.send('login user')
+    const {email, password} = req.body
+    if (!email || !password) {
+        throw new BadRequestError('Please provide email and password')
+    }
+
+    const user = await User.findOne({email})
+    if (!user) {
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password)
+    if (!isPasswordCorrect) {
+        throw new UnauthenticatedError('Invalid Credentials')
+    }
+
+    const tocken = user.createJWT()
+    res.status(StatusCodes.OK).json({user: {name: user.name}, tocken})
 }
+
 
 module.exports = {
     register,
