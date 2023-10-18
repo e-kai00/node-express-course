518f3c055fe63c341b53bafd6774bcb214957225
Katerina <yelkaterina@gmail.com>
2023-10-18 17:50:41 +0200

    Add feat: get single job

--- 06-jobs-api/starter/controllers/jobs.js
+++ 06-jobs-api/starter/controllers/jobs.js
@@ -10,7 +10,17 @@ const getAllJobs = async (req, res) => {
 }
 
 const getJob = async (req, res) => {
-    res.send('get a single job')
+    // user object - through middleware, params - through express
+    const {user: {userId}, params: {id: jobId}} = req
+
+    const job = await Job.findOne({_id: jobId, createdBy: userId})
+
+    if (!job) {
+        throw new NotFoundError(`No job with id ${jobId}`)
+    }
+
+    res.status(StatusCodes.OK).json({job})
+
 }
 
 const createJob = async (req, res) => {
