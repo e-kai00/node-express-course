c4d8bee324650a622967f22a6c85007e7ab3f0bd
Katerina <yelkaterina@gmail.com>
2023-11-01 16:56:46 +0100

    Feat: pagination

--- 06.5-jobster-api/starter/controllers/jobs.js
+++ 06.5-jobster-api/starter/controllers/jobs.js
@@ -9,6 +9,7 @@ const getAllJobs = async (req, res) => {
   const queryObject = {
     createdBy: req.user.userId
   }  
+  // filter
   if (search) {
     queryObject.position = {$regex: search, $options: 'i'}
   }
@@ -21,6 +22,7 @@ const getAllJobs = async (req, res) => {
 
   let result = Job.find(queryObject)
 
+  // sort
   if (sort === 'latest') {
     result = result.sort('-createdAt')
   }
@@ -34,8 +36,19 @@ const getAllJobs = async (req, res) => {
     result = result.sort('-position')
   }
 
+  // pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page -1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const jobs = await result
-  res.status(StatusCodes.OK).json({ jobs })
+
+  const totalJobs = await Job.countDocuments(queryObject)
+  const numOfPages = Math.ceil(totalJobs / limit)
+
+  res.status(StatusCodes.OK).json({ jobs, totalJobs, numOfPages })
 }
 
 const getJob = async (req, res) => {
