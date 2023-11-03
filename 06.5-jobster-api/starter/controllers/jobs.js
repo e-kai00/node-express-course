b52717eea13edd16f45831bf514d3f3e1e22e783
Katerina <yelkaterina@gmail.com>
2023-11-03 16:58:53 +0100

    Feat: monthly applications stats

--- 06.5-jobster-api/starter/controllers/jobs.js
+++ 06.5-jobster-api/starter/controllers/jobs.js
@@ -130,7 +130,20 @@ const showStats = async (req, res) => {
     declined: stats.declined || 0,
   }
 
-  console.log(stats)
+  let monthlyApplications = await Job.aggregate([
+    {$match: {createdBy: mongoose.Types.ObjectId(req.user.userId)}},
+    {
+      $group: {
+        _id: {year: {$year: '$createdAt'}, month: {$month: '$createdAt'}},
+        count: {$sum: 1}
+      }
+    },
+    {$sort: {'_id.year': -1, 'id_month': -1}},
+    {$limit: 6}
+  ])
+
+
+  console.log(monthlyApplications)
 
   res
     .status(StatusCodes.OK)
