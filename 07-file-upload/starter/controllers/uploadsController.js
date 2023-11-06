8c8422dba95b26fdb59a2797f16d3d1bb2462b43
Katerina <yelkaterina@gmail.com>
2023-11-06 18:33:19 +0100

    Complete file upload feat

--- 07-file-upload/starter/controllers/uploadsController.js
+++ 07-file-upload/starter/controllers/uploadsController.js
@@ -1,10 +1,23 @@
 const {StatusCodes} = require('http-status-codes')
 const path = require('path')
+const CustomError = require('../errors')
 
 
 const uploadProductImage = async (req, res) => {
-    // console.log(req.files)
-    let productImage = req.files.image;
+    // check if file exist
+    if (!req.files) {
+        throw new CustomError.BadRequestError('No File Uploaded');
+    };     
+    const productImage = req.files.image;
+    // check format
+    if (!productImage.mimetype.startsWith('image')) {
+        throw new CustomError.BadRequestError('Uploaded Image');
+    };
+    // check size
+    const maxSize = 1024 * 1024;
+    if (productImage.size > maxSize) {
+        throw new CustomError.BadRequestError('Please upload image smaller image');
+    };
 
     // construct path
     const imagePath = path.join(__dirname, '../public/uploads/' + `${productImage.name}`);
