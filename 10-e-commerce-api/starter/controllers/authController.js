e99fe1542b709d744ad8a96da912bdbc480a3bfe
Katerina <yelkaterina@gmail.com>
2023-11-09 16:00:51 +0100

    Handle token with cookie

--- 10-e-commerce-api/starter/controllers/authController.js
+++ 10-e-commerce-api/starter/controllers/authController.js
@@ -23,8 +23,14 @@ const register = async (req, res) => {
     // issue tocken
     const tokenUser = {name: user.name, userId: user._id, role: user.role};
     const token = createJWT({payload: tokenUser});
+    // token via cookie
+    const oneDay = 1000 * 60 * 60 * 24;
+    res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + oneDay),
+    })
 
-    res.status(StatusCodes.CREATED).json({user: tokenUser, token});
+    res.status(StatusCodes.CREATED).json({user: tokenUser});
 };
 
 const login = async (req, res) => {
