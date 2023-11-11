549f2dd37d2ed82682c86d2b186d6e745f92b034
Katerina <yelkaterina@gmail.com>
2023-11-11 19:22:33 +0100

    Feat: authenticate user

--- 10-e-commerce-api/starter/controllers/userController.js
+++ 10-e-commerce-api/starter/controllers/userController.js
@@ -4,8 +4,9 @@ const CustomError = require('../errors');
 
 
 const getAllUsers = async (req, res) => {
+    console.log(req.user)
+
     const users = await User.find({role: 'user'}).select('-password');
-    console.log(users)
     res.status(StatusCodes.OK).json({users});
 };
 
--- 10-e-commerce-api/starter/routes/userRoutes.js
+++ 10-e-commerce-api/starter/routes/userRoutes.js
@@ -7,13 +7,14 @@ const {
     updateUser,
     updateUserPassword,
 } = require('../controllers/userController');
+const {authenticateUser} = require('../middleware/authentication')
 
 
-router.route('/').get(getAllUsers);
+router.route('/').get(authenticateUser, getAllUsers);
 router.route('/showMe').get(showCurrentUser);
 router.route('/updateUser').patch(updateUser);
 router.route('/updateUserPassword').patch(updateUserPassword);
-router.route('/:id').get(getSingleUser);
+router.route('/:id').get(authenticateUser, getSingleUser);
 
 
 module.exports = router;
