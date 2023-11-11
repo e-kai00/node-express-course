683a0cceea65d50fff58338e9c853f7e61c4fa44
Katerina <yelkaterina@gmail.com>
2023-11-11 20:00:28 +0100

    Set up authorized permissions

--- 10-e-commerce-api/starter/middleware/authentication.js
+++ 10-e-commerce-api/starter/middleware/authentication.js
@@ -8,7 +8,7 @@ const authenticateUser = async (req, res, next) => {
     if (!token) {
       throw new CustomError.UnauthenticatedError('Authentication Invalid');
     }
-  
+
     try {
       const { name, userId, role } = isTokenValid({ token });
       req.user = { name, userId, role };
@@ -18,7 +18,25 @@ const authenticateUser = async (req, res, next) => {
     }
   };
 
+// const authorizePermissions = (req, res, next) => {
+//     if (req.user.role !== 'admin') {
+//         throw new CustomError.UnauthorizedError('Unauthorized to access this route')
+//     }
+//     next();
+// }
+
+// '...role' - rest operator; roles arg comes from userRoutes.js when invoking authorizePermissions(role1, role2)
+const authorizePermissions = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            throw new CustomError.UnauthorizedError('Unauthorized to access this route');
+        }
+        next();
+    };
+};
+
 
 module.exports = {
     authenticateUser,
+    authorizePermissions,
 }
