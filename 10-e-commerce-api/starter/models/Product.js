7c750716c1553a3450be0568ff68d65cb0bb5603
Katerina <yelkaterina@gmail.com>
2023-11-12 16:56:27 +0100

    Add Product model

---
+++ 10-e-commerce-api/starter/models/Product.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+
+const ProductSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: [true, 'Provide product name'],
+            maxlength: [100, 'Product name cannot be longer than 100 characters'],
+        },
+        price: {
+            type: Number,
+            required: [true, 'Provide product price'],
+            default: 0,
+        },
+        desription: {
+            type: String,
+            required: [true, 'Provide product description'],            
+            maxlength: [1000, 'Product description cannot be longer than 1000 characters'],
+        },
+        image: {
+            type: String,
+            default: '/uploads/example.jpeg'
+        },
+        category: {
+            type: String,
+            required: [true, 'Provide product category'],        
+            enum: ['office', 'kitchen', 'bedroom'],
+        },
+        company: {
+            type: String,
+            required: [true, 'Provide company'],
+            enum: {
+                values: ['ikea', 'liddy', 'marcos'],
+                message: '{VALUE} is not supported.'
+            },
+        },
+        colors: {
+            type: [String],
+            required: true,
+        },
+        featured: {
+            type: Boolean,
+            default: false,
+        },
+        freeShipping: {
+            type: Boolean,
+            default: false,
+        },
+        inventory: {
+            type: Number,
+            required: true,
+            default: 15,
+        },
+        averageRating: {
+            type: Number,
+            default: 0,
+        },
+        user: {
+            type: mongoose.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+    },
+    { timestamps: true }   
+);
+
+
+module.exports = mongoose.model('Product', ProductSchema);
